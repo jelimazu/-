6ed0e0052c669e6c02c068adaf4b9a0a49fa8b0e
jelimazu <52045982+jelimazu@users.noreply.github.com>
2024-12-01 22:56:46 +0100

    lab1

---
+++ lab1/server/app/models.py
@@ -0,0 +1,15 @@
+from . import db
+
+class Meeting(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    date = db.Column(db.DateTime, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "date": self.date.isoformat()
+        }
